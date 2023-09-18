628b8607fd91b783892768d40a130017bc18ce9d
alina <alina.svetlaya@gmail.com>
2023-09-18 21:08:13 +0200

    added login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,9 @@
+import {LoginForm} from "@/components/loginForm";
+
 export default function Login () {
-    return <div>LOGIN</div>
+    return <div>LOGIN
+    <div>
+        <LoginForm/>
+    </div>
+    </div>
 }
