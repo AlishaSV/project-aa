2996283138095844e2a2dbb16b3c48bf516d48fd
alina <alina.svetlaya@gmail.com>
2023-09-19 16:55:18 +0200

    added common styles for my account

---
+++ src/components/styledComponents/account/styledAccount.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import styled from "@emotion/styled";
+
+export const StyledAccount = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 17px;
+  line-height: 1.23536;
+  font-weight: 400;
+  letter-spacing: -.022em;
+  font-family: SF Pro Text,SF Pro Icons,AOS Icons,Helvetica Neue,Helvetica,Arial,sans-serif;
+  color: #6e6e73;
+`;
+
+export const StyledHeader = styled.div`
+  font-size: 32px;
+  line-height: 1.08349;
+  font-weight: 600;
+  letter-spacing: -.003em;
+  font-family: SF Pro Display, SF Pro Icons, AOS Icons, Helvetica Neue, Helvetica, Arial, sans-serif;
+  margin-bottom: 20px;
+  margin-top: 20px;
+  color: rgb(73, 77, 77);
+`
