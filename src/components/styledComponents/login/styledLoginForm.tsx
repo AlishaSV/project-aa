33ea77adb82fabb51e3dc4659664ec65c95c43d4
alina <alina.svetlaya@gmail.com>
2023-09-19 16:53:57 +0200

    added styles for login form

---
+++ src/components/styledComponents/login/styledLoginForm.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import styled from "@emotion/styled";
+import {Form} from "formik";
+
+export const StyledLoginForm = styled(Form)`
+  display: flex;
+  color: darkslateblue;
+  font-weight: bold;
+  align-content: flex-start;
+  align-items: flex-start;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+`;
+
