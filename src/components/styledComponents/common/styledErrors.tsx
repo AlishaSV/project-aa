6a79eaf5020c655cf0330ffd4843cc54a4c2612b
alina <alina.svetlaya@gmail.com>
2023-09-19 16:54:51 +0200

    added common styles for errors, headers and wrappers

---
+++ src/components/styledComponents/common/styledErrors.tsx
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const StyledErrors = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  color: red;
+  font-family: inherit;
+  font-size: small;
+`
