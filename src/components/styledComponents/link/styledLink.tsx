c4c5d67ef0acb72456f000036cc3fab8d0b5c6a0
alina <alina.svetlaya@gmail.com>
2023-09-19 16:54:20 +0200

    added styles for nav links

---
+++ src/components/styledComponents/link/styledLink.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import Link from "next/link";
+import styled from "@emotion/styled";
+
+export const StyledLink = styled(Link)`
+  color: #2d2e2e;
+  font-size: 20px;
+  font-size: 1.33333vw;
+  font-family: "Twentieth Century W01", Futura, Helvetica, Arial, sans-serif;
+  font-weight: 500;
+  letter-spacing: .01em;
+  line-height: 1em;
+  padding-bottom: 1.6vw;
+  text-transform: inherit;
+`;
